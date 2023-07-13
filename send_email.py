d0c4af5ea3775e6c5b221dc64689edbc02c3126c
Rebwar Me <133606750+rebwar-me@users.noreply.github.com>
2023-07-13 18:32:31 +0330

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,3 +1,4 @@
+#author : @rebwar_ai  @rebwar_me
 from flask import Flask,render_template
 from flask_mail import Mail,Message
 
@@ -12,9 +13,9 @@ app.config['MAIL_PORT'] = 587
 
 app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = "tx369t@gmail.com"
+app.config['MAIL_USERNAME'] = "your_gmail@gmail.com"
 
-app.config['MAIL_PASSWORD'] = "ftyfdxbbtjxxqvsi"
+app.config['MAIL_PASSWORD'] = "your_app_password"
 
 mail = Mail(app)
 
